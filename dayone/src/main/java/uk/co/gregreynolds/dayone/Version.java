ea1d0e7e50faa1cc6c2dfd7da589c7969dc1916f
Greg Reynolds <dr.greg.reynolds@googlemail.com>
2013-08-17 02:54:17 +0300

    Messing around with version file

--- dayone/src/main/java/uk/co/gregreynolds/dayone/Version.java
+++ dayone/src/main/java/uk/co/gregreynolds/dayone/Version.java
@@ -4,7 +4,7 @@ public final class Version {
 
     public static final String build_number="";
 
-    public static final String build_time="20130817-0231";
+    public static final String build_time="20130817-0252";
 
     public static final String pomversion="0.0.2-SNAPSHOT";
 
