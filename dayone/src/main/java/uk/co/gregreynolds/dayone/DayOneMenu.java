0e076150dbc0cecb774c60c26f8850fa23e6b565
Greg Reynolds <dr.greg.reynolds@googlemail.com>
2013-08-17 02:33:26 +0300

    Added menus

---
+++ dayone/src/main/java/uk/co/gregreynolds/dayone/DayOneMenu.java
@@ -0,0 +1,75 @@
+package uk.co.gregreynolds.dayone;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+
+public class DayOneMenu extends JMenuBar 
+{
+  private JMenu fileMenu;
+  private JMenu helpMenu;
+  private JMenuItem directoryLocationItem;
+  
+  
+  public DayOneMenu(final Component parent)
+  {
+    super();
+    
+    fileMenu = new JMenu("File");
+    fileMenu.setMnemonic(KeyEvent.VK_F);
+    
+    directoryLocationItem = new JMenuItem("Set Directory");
+    directoryLocationItem.setMnemonic(KeyEvent.VK_D);
+    directoryLocationItem.setToolTipText("Set the Journal Directory");
+    
+    JMenuItem eMenuItem = new JMenuItem("Exit");
+    eMenuItem.setMnemonic(KeyEvent.VK_E);
+    eMenuItem.setToolTipText("Exit application");
+    eMenuItem.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+            System.exit(0);
+        }
+    });
+    
+    fileMenu.add(directoryLocationItem);
+    fileMenu.addSeparator();
+    fileMenu.add(eMenuItem);
+    
+    helpMenu = new JMenu("Help");
+    helpMenu.setMnemonic(KeyEvent.VK_H);    
+    JMenuItem aboutItem = new JMenuItem("About");
+    aboutItem.getAccessibleContext().setAccessibleDescription(
+        "Display the about text");
+    aboutItem.addActionListener(new ActionListener()
+    {
+      
+      @Override
+      public void actionPerformed(ActionEvent e)
+      {
+        String message = "DayOneViewer\n© Greg Reynolds\nVersion: " + Version.pomversion +
+            "\nBuild time: " + Version.build_time;
+        JOptionPane.showMessageDialog(parent, message,"About",JOptionPane.INFORMATION_MESSAGE);
+      }
+    });
+    helpMenu.add(aboutItem);
+    
+    
+    add(fileMenu);
+    
+    
+    add(helpMenu);
+  }
+  
+  JMenuItem getDirectoryLocationItem()
+  {
+    return directoryLocationItem;
+  }
+  
+}
